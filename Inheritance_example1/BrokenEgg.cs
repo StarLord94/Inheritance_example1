d8c73ad34f7e39e4c4904a27a599d80cf724d966
Mykola Kuzyk <ukrboy99@gmail.com>
2021-08-07 01:44:52 +0200

    Broken Egg functionality added

---
+++ Inheritance_example1/BrokenEgg.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance_example1
+{
+    class BrokenEgg:Egg
+    {
+        public BrokenEgg(string color) : base(0,$"Broken {color}")
+        {
+            Console.WriteLine("A bird laid a broken egg ");
+        }
+    }
+}
--- Inheritance_example1/Pigeon.cs
+++ Inheritance_example1/Pigeon.cs
@@ -8,12 +8,17 @@ namespace Inheritance_example1
     {
         public override Egg[] LayEggs(int numberOfEggs)
 
+             
         {
             Egg[] eggs = new Egg[numberOfEggs];
             for (int i = 0; i < numberOfEggs; i++)
             {
-                eggs[i] = new Egg(Bird.Randomizer.NextDouble() * 2 + 1, "White");
+                if (Bird.Randomizer.Next(4) == 0)
+                    eggs[i] = new BrokenEgg("White");
+                else
+                    eggs[i] = new Egg(Bird.Randomizer.NextDouble() * 2 + 1, "White");
             }
+
             return eggs;
 
         }
